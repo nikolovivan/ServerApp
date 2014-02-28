04e1244e69922a76e661c0f5f89d9bb2f073c3ac
Ivan Nikolov <nikolovivann@gmail.com>
2014-02-28 23:05:36 +0000

    Fixed a typo, that didn't actually break anything.

--- src/main/java/com/ivan/nikolov/telnet/start/StartServer.java
+++ src/main/java/com/ivan/nikolov/telnet/start/StartServer.java
@@ -16,35 +16,35 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class StartServer {
 
-	/**
-	 * @param args
-	 * @throws Exception
-	 *             if the initialization of the server was not successful.
-	 */
-	public static void main(final String[] args) throws Exception {
-		Server server = new Server(8080);
+    /**
+     * @param args
+     * @throws Exception
+     *             if the initialization of the server was not successful.
+     */
+    public static void main(final String[] args) throws Exception {
+        Server server = new Server(8080);
 
-		WebAppContext context = new WebAppContext();
-		context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
-		context.setResourceBase("./src/main/webapp");
-		context.setContextPath("/");
-		context.setParentLoaderPriority(true);
+        WebAppContext context = new WebAppContext();
+        context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
+        context.setResourceBase("./src/main/webapp");
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
 
-		RequestLogHandler requestLogHandler = new RequestLogHandler();
-		NCSARequestLog requestLog = new NCSARequestLog("./src/main/webapp/logs/jetty-yyyy_mm_dd.request.log");
-		requestLog.setRetainDays(90);
-		requestLog.setAppend(true);
-		requestLog.setExtended(true);
-		requestLog.setLogTimeZone("GMT");
-		requestLogHandler.setRequestLog(requestLog);
+        RequestLogHandler requestLogHandler = new RequestLogHandler();
+        NCSARequestLog requestLog = new NCSARequestLog("./src/main/webapp/logs/jetty-yyyy_mm_dd.request.log");
+        requestLog.setRetainDays(90);
+        requestLog.setAppend(true);
+        requestLog.setExtended(true);
+        requestLog.setLogTimeZone("GMT");
+        requestLogHandler.setRequestLog(requestLog);
 
-		HandlerCollection handlers = new HandlerCollection();
-		handlers.setHandlers(new Handler[] { context, requestLogHandler });
+        HandlerCollection handlers = new HandlerCollection();
+        handlers.setHandlers(new Handler[] { context, requestLogHandler });
 
-		server.setHandler(handlers);
+        server.setHandler(handlers);
 
-		server.start();
-		server.join();
-	}
+        server.start();
+        server.join();
+    }
 
 }
